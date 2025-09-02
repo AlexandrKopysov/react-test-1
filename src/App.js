87a3ed0e98641b1a6d1b9a095aa6dd302ee466b0
Александр Копысов <kopav@ite.lan@ite-mp-00441.ite.lan>
2025-09-02 13:13:22 +0500

    Стартовое приложение React с датой

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
+// Императивный стиль
+const date = new Date().getFullYear().toString()
+
+// Деклоративный стиль 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<p>
+					{date}
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
